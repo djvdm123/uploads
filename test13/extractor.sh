9fd60413ac6f03fa3745f0a83d4db3da6ba829b4
vagrant-up.com <vagrant@precise-yaffs.(none)>
2013-12-17 20:09:46 +0000

    start

---
+++ test13/extractor.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+file=et.txt
+
+echo " "
+echo "n_page_writes"
+cat $file | grep "n_page_writes" | cut -d" " -f2
+
+echo " "
+echo "n_page_reads"
+cat $file | grep "n_page_reads" | cut -d" " -f2
+
+echo " "
+echo "n_erasures"
+cat $file | grep "n_erasures" | cut -d" " -f2
+
+echo " "
+echo "all_gcs"
+cat $file | grep "all_gcs" | cut -d" " -f2
+
+echo " "
+echo "passive_gc_count"
+cat $file | grep "passive_gc_count" | cut -d" " -f2
+
+echo " "
+echo "bg_gcs"
+cat $file | grep "bg_gcs" | cut -d" " -f2
